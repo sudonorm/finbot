2b6e6ada54afcd9e1ec5aaebc88aa7799298d2bf
sudonorm <ayo.toba@gmail.com>
2024-10-15 23:34:29 -0600

    add main FastAPI app

---
+++ app/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, responses
+from fastapi.routing import APIRoute
+from starlette.middleware.cors import CORSMiddleware
+
+from app.api.api_v1.api import api_router
+from core.config import settings
+
+from fastapi.staticfiles import StaticFiles
+
+
+def custom_generate_unique_id(route: APIRoute):
+    return f"{route.tags[0]}-{route.name}"
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    generate_unique_id_function=custom_generate_unique_id,
+    default_response_class=responses.ORJSONResponse,
+    # root_path="/api/v1",
+)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Set all CORS enabled origins
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
